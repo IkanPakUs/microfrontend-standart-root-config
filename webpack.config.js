595d11ee26faa52e1f08378809595d64424aea61
IkanPakUs <komanggede86@gmail.com>
2023-01-17 16:16:24 +0800

    First setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "ikanpakus";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        }
+      ]
+    }
+  });
+};
